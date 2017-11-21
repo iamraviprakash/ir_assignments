9ecc8a210fb2932034fde4254c9fd65879ab0cbc
Soham Marik <soham.m15@iiits.in>
2017-11-21 19:58:05 +0530

    Some more progress in searching for inverted index

--- search_doc.py
+++ search_doc.py
@@ -5,32 +5,19 @@ import time
 from nltk import PorterStemmer
 
 f=open('data/index_table.json','r')
-#start=time.time()
-ind_table=json.load(f)
-for word in ind_table.keys():
-	if word=='both' or word=='Both':
-		print word 
-#end=time.time()
-#print end-start
+index_table=json.load(f)
 
 search_term=raw_input("Enter search term:")
 search_words=search_term.split()
 
-doc_list=[]
 stemmer=PorterStemmer()
 
 first_word=stemmer.stem(search_words[0])
-if first_word in ind_table and ind_table[first_word] not in doc_list:
-	#print ind_table[first_word]
-	doc_list+=ind_table[first_word]
+doc_list=index_table[first_word]
 
-#print doc_list
-
-'''for word in search_words:
+for word in search_words:
 	word=stemmer.stem(word)
-	if word in ind_table and ind_table[word] not in doc_list:
-		doc_list+=ind_table[word]'''
+	doc_list2=index_table[word]
+	doc_list=list(set(doc_list) & set(doc_list2))
 
-for doc in doc_list:
-	f=open(doc)
-	#print ('\n').join(f.readlines())
+print doc_list
